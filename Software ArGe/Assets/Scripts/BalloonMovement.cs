274e51e993f776b9780f7965e2f70b294c766234
Nisa Dost <i.n.dost38@gmail.com>
2023-08-04 01:52:54 +0300

    pop mechanics added
    ballons pop when clicked
    idle animations added
    
    to do list:
    timer
    score system
    balloon health system
    accelerate speed over time

--- Software ArGe/Assets/Scripts/BalloonMovement.cs
+++ Software ArGe/Assets/Scripts/BalloonMovement.cs
@@ -6,22 +6,62 @@ public class BalloonMovement : MonoBehaviour
 {
     [SerializeField] Vector3 force;
     [SerializeField] GameObject[] balloons;
+    private RuntimeAnimatorController animatorController;
     private GameObject randomBalloons;
     private SpriteRenderer sr;
     private Rigidbody2D rb;
+    private Animator anim;
+
+    int redHealth = 3;
+    int yellowHealth = 2;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         randomBalloons = GetComponent<GameObject>();
         sr = GetComponent<SpriteRenderer>();
-        
-        sr.sprite = balloons[Random.Range(0, balloons.Length)].GetComponent<SpriteRenderer>().sprite;
+        anim = GetComponent<Animator>();
+
+        int balloonNo = Random.Range(0, balloons.Length);
+
+        anim.runtimeAnimatorController = balloons[balloonNo].GetComponent<Animator>().runtimeAnimatorController;
+
+        sr.sprite = balloons[balloonNo].GetComponent<SpriteRenderer>().sprite;
         transform.position = new Vector3(Random.Range(-2.5f, 2.5f), transform.position.y, transform.position.z);
 
         force = new Vector3(Random.Range(-100, 100), Random.Range(100, 200), 0);
         rb.AddForce(force);
-        
+
+    }
+
+    private void OnMouseDown() { //balonlara health sistemi eklenecek - farklı scriptte de olabilir
+        // if (gameObject.CompareTag("BlueBalloon"))
+        // {
+        //     PopBalloon();
+        // }
+        // if (gameObject.CompareTag("RedBalloon"))
+        // {
+        //     redHealth--;
+        //     if (redHealth <= 0)
+        //     {
+        //         PopBalloon();
+        //     }
+        // }
+        // if (gameObject.CompareTag("YellowBalloon"))
+        // {
+        //     yellowHealth--;
+        //     if (yellowHealth <= 0)
+        //     {
+        //         PopBalloon();
+        //     }
+        // }
+        PopBalloon();
+    }
+
+    void PopBalloon()
+    {
+        anim.SetTrigger("Pop");
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
--- Software ArGe/Assets/Scripts/BalloonSpawner.cs
+++ Software ArGe/Assets/Scripts/BalloonSpawner.cs
@@ -5,26 +5,12 @@ using UnityEngine;
 public class BalloonSpawner : MonoBehaviour
 {
     [SerializeField] GameObject balloonPrefab;
-    [SerializeField] private Animator anim;
-    // Start is called before the first frame update
+
     void Start()
     {
-        anim = GetComponent<Animator>();
         InvokeRepeating("SpawnBalloon", 0f, 1f);
     }
 
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (gameObject.tag == "Balloon")
-            {
-                anim.SetTrigger("Pop");
-            }
-        }
-    }
     void SpawnBalloon()
     {
         Instantiate(balloonPrefab, transform.position, Quaternion.identity);
