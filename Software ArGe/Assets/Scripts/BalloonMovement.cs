17e969bb6e3274c1009cb889c3b5751eaf1795ac
Nisa Dost <i.n.dost38@gmail.com>
2023-08-03 21:50:24 +0300

    spawn & destroyer mechanics added
    destroy area added
    balloon spawner added
    
    to do list:
    add pop animations when clicked
    add destroy balloon event on animations

--- Software ArGe/Assets/Scripts/BalloonMovement.cs
+++ Software ArGe/Assets/Scripts/BalloonMovement.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class BalloonMovement : MonoBehaviour
 {
     [SerializeField] Vector3 force;
-    [SerializeField] Sprite[] balloons;
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] GameObject[] balloons;
+    private GameObject randomBalloons;
+    private SpriteRenderer sr;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        randomBalloons = GetComponent<GameObject>();
+        sr = GetComponent<SpriteRenderer>();
+        
+        sr.sprite = balloons[Random.Range(0, balloons.Length)].GetComponent<SpriteRenderer>().sprite;
+        transform.position = new Vector3(Random.Range(-2.5f, 2.5f), transform.position.y, transform.position.z);
 
         force = new Vector3(Random.Range(-100, 100), Random.Range(100, 200), 0);
         rb.AddForce(force);
-        spriteRenderer.sprite = balloons[Random.Range(0, balloons.Length)];
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other) 
     {
-        
+        if (other.gameObject.tag == "DestroyArea")
+        {
+            Destroy(gameObject);
+        }
     }
 }
